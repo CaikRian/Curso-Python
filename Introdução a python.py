4a4f64e1c762af928826f5d1d64f8197626d3760
Caik Rian <84456291+CaikRian@users.noreply.github.com>
2023-03-03 10:52:31 -0300

    Primeira Aula de python

---
+++ Introdução a python.py
@@ -0,0 +1,23 @@
+
+# Para mostrar na tela alguma "saída de informação", precisa digitar "print"
+
+nome = 'Caik'
+idade = 19
+peso = 65.4
+print('Meu nome é', nome + '. Tenho', idade, 'anos e', peso +'Kg')
+
+
+#input serve para mandar o usuário digitar 
+
+nome = input("Qual é o seu nome?\n")
+idade = input("Qual é a sua idade?\n")
+peso = input("Qual é o seu peso?\n")
+print('Meu nome é', nome + '. Tenho', idade, 'anos e', peso +'Kg')
+
+#Determinando o tipo da variável
+
+n1 = int (input("Digite um primeiro número:\n"))
+n2 = int (input("Digite um segundo número:\n"))
+soma = n1 + n2 
+print( "A soma é: ", soma)
+
