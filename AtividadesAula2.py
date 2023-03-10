72b0550d71627cb4f6e4a777b8fd9577e5cf8273
Caik Rian <84456291+CaikRian@users.noreply.github.com>
2023-03-10 11:01:34 -0300

    utilizando if else

--- AtividadesAula2.py
+++ AtividadesAula2.py
@@ -1,38 +1,39 @@
-#num = float(input("Digite um número: "))
+#--- exercicio 1
 
-#if num > 0 and num < 9:
-#    print("Valor correto!")
-#else:
-#    print("Valor incorreto!")
+num = float(input("Digite um número: "))
+if num > 0 and num < 9:
+    print("Valor correto!")
+else:
+   print("Valor incorreto!")
 
 #--- exercicio 2
 
-#turno = str(input("Digite o seu turno (N) ou (M): "))
-#qtdhoras = int(input("Digite a quantidade de horas trabalhadas: "))
+turno = str(input("Digite o seu turno (N) ou (M): "))
+qtdhoras = int(input("Digite a quantidade de horas trabalhadas: "))
 
-#if turno == "N" or turno == "n":
+if turno == "N" or turno == "n":
 
-#    print("Valor da hora trabalhada: R$45,00\n")
-#    print("Seu salário final: R$%.2f" %(qtdhoras*45))
+   print("Valor da hora trabalhada: R$45,00\n")
+   print("Seu salário final: R$%.2f" %(qtdhoras*45))
 
-#elif turno == "M" or turno == "m":
+elif turno == "M" or turno == "m":
 
-#    print("Valor da hora trabalhada: R$37,50\n")
-#    print("Seu salário final: R$%.2f" %(qtdhoras*37.50))
+   print("Valor da hora trabalhada: R$37,50\n")
+   print("Seu salário final: R$%.2f" %(qtdhoras*37.50))
 
 
 
 #exercício 3
 
-#compra = float(input("Digite o valor da sua compra: "))
+compra = float(input("Digite o valor da sua compra: "))
 
-#if compra > 200:
-#    print("Você ganhou um desconto de 20%! \n")
-#    print("Sua compra foi de R$%.2f" % compra)
-#   print("para R$%.2f" % (compra-(compra*0.20)))
+if compra > 200:
+   print("Você ganhou um desconto de 20%! \n")
+   print("Sua compra foi de R$%.2f" % compra)
+  print("para R$%.2f" % (compra-(compra*0.20)))
 
-#else:
-#    print("Sua compra não passou de R$200 para conseguir o desconto.")
+else:
+   print("Sua compra não passou de R$200 para conseguir o desconto.")
 
 
 
