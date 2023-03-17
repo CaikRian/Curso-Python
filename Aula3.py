eb30dce32bec934dd3cf84a5fd8c9151675fb425
Caik Rian <84456291+CaikRian@users.noreply.github.com>
2023-03-17 10:32:42 -0300

    Utilizando biblioteca Import Math

--- Aula3.py
+++ Aula3.py
@@ -1,45 +1,45 @@
 #exemplo 1
 
-#num = int(input("Digite um número com três digitos: "))
-#d1 = num // 100
-#d2 = num % 100 // 10
-#d3 = num % 10
-#inverso = d3*100+d2*10+d1
-#print("O inverso do número digitado é: ", inverso)
+num = int(input("Digite um número com três digitos: "))
+d1 = num // 100
+d2 = num % 100 // 10
+d3 = num % 10
+inverso = d3*100+d2*10+d1
+print("O inverso do número digitado é: ", inverso)
 
 
 
 #exemplo 2
 
-#import math
-#num = float(input("Digite um número: "))
-#resultado = math.sqrt(num)
-#print("O valor da raiz quadrada é: ", resultado)
+import math
+num = float(input("Digite um número: "))
+resultado = math.sqrt(num)
+print("O valor da raiz quadrada é: ", resultado)
 
 
 #exemplo 3
 
-#import math
-#num = float(input("Digite um número real: "))
-#absoluto = math.fabs(num)
-#inteiro = math.trunc(num)
-#raiz = math.sqrt(absoluto)
-#fatorial = math.factorial(math.fabs(inteiro))
+import math
+num = float(input("Digite um número real: "))
+absoluto = math.fabs(num)
+inteiro = math.trunc(num)
+raiz = math.sqrt(absoluto)
+fatorial = math.factorial(math.fabs(inteiro))
 
-#print("Absoluto: ", absoluto)
-#print("Inteiro: ", inteiro)
-#print("Raiz: ", raiz)
-#print("Fatorial: ", fatorial)
+print("Absoluto: ", absoluto)
+print("Inteiro: ", inteiro)
+print("Raiz: ", raiz)
+print("Fatorial: ", fatorial)
 
 
 #exemplo 4
 
-#import math
-#raio = float(input("Digite o raio da circuferência em cm: "))
-#comprimento = 2 * math.pi * raio
-#area = math.pi * raio * raio
-#print("O comprimento da circuferência é igual a %.2f cm\n" % (comprimento))
-#print("A área da circuferência é igual a %.2f cm²" %(area))
+import math
+raio = float(input("Digite o raio da circuferência em cm: "))
+comprimento = 2 * math.pi * raio
+area = math.pi * raio * raio
+print("O comprimento da circuferência é igual a %.2f cm\n" % (comprimento))
+print("A área da circuferência é igual a %.2f cm²" %(area))
 
 
 
